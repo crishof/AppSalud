1a4a9c3deea18a6d2889fa8804ccbd49e6ecbd6e
Cristian Hoffmann <cristianhof@gmail.com>
2023-11-22 11:05:47 +0100

    muevo post de registro paciente

--- src/main/java/com/egg/appsalud/controladores/PacienteController.java
+++ src/main/java/com/egg/appsalud/controladores/PacienteController.java
@@ -1,4 +1,48 @@
 package com.egg.appsalud.controladores;
 
+import com.egg.appsalud.Exception.MiException;
+import com.egg.appsalud.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/paciente")
 public class PacienteController {
+
+    @Autowired
+    UsuarioServicio usuarioServicio;
+
+    @PostMapping("/registrar")
+    public String registrarPaciente(/*@RequestParam MultipartFile archivo,*/@RequestParam String nombreUsuario, @RequestParam String nombre, @RequestParam String apellido, @RequestParam(required = false) Long dni, @RequestParam("fechaDeNacimiento") String fechaDeNacimientoStr, @RequestParam String email, @RequestParam String password, @RequestParam String password2, ModelMap modelo) throws MiException, ParseException {
+        Date fechaDeNacimiento;
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            fechaDeNacimiento = dateFormat.parse(fechaDeNacimientoStr);
+
+        } catch (ParseException p) {
+            modelo.put("error", "la fecha no puede venir vacía");
+            return "redirect:/portal/registroUsuario";
+        }
+
+        try {
+
+            usuarioServicio.crearUsuario(/*archivo, */nombreUsuario, nombre, apellido, dni, fechaDeNacimiento, email, password, password2);
+
+            modelo.put("exito", "el usuario fue creado con exito");
+            return "index";
+        } catch (MiException e) {
+
+            modelo.put("error", e.getMessage());
+
+            return "redirect:/portal/registroUsuario";
+        }
+    }
 }
