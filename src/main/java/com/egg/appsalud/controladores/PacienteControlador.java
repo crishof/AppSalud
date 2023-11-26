ca84b759e2b3848b5328456313b9880c7e4287f1
crishof <cristianhof@gmail.com>
2023-11-26 10:21:42 +0100

    cita paciente

--- src/main/java/com/egg/appsalud/controladores/PacienteControlador.java
+++ src/main/java/com/egg/appsalud/controladores/PacienteControlador.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
@@ -31,6 +29,17 @@ public class PacienteControlador {
         return "cita_solicitud";
     }
 
+    @PostMapping("/solicitar/{id}")
+    public String  reservarCita(@PathVariable String id, @RequestParam String idProfesional, ModelMap model){
+
+        model.addAttribute("paciente",pacienteServicio.getOne(id));
+        model.addAttribute("profesional",profesionalServicio.getOne(idProfesional));
+
+
+        return "redirect:../citas";
+    }
+
+
     @GetMapping("/editar")
     public String editarPaciente(){
         return "paciente_edit";
@@ -38,7 +47,7 @@ public class PacienteControlador {
 
     @GetMapping("/citas")
     public String listarCitas() {
-        return "citasPaciente";
+        return "paciente_citas";
     }
 
     @GetMapping("/historia")
