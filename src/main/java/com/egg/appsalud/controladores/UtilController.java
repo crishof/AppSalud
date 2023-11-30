bfd3fafb824f1d450df3787c149ea22cb53a4155
Cristian Hoffmann <cristianhof@gmail.com>
2023-11-30 17:00:57 +0100

    metodo automatico paciente

--- src/main/java/com/egg/appsalud/controladores/UtilController.java
+++ src/main/java/com/egg/appsalud/controladores/UtilController.java
@@ -4,7 +4,10 @@ import com.egg.appsalud.Enumeracion.DiaSemana;
 import com.egg.appsalud.Enumeracion.Especialidad;
 import com.egg.appsalud.Enumeracion.Provincias;
 import com.egg.appsalud.Exception.MiException;
+import com.egg.appsalud.entidades.Paciente;
+import com.egg.appsalud.repositorios.PacienteRepositorio;
 import com.egg.appsalud.repositorios.ProfesionalRepositorio;
+import com.egg.appsalud.servicios.PacienteServicio;
 import com.egg.appsalud.servicios.ProfesionalServicio;
 import com.egg.appsalud.servicios.TurnoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +31,13 @@ public class UtilController {
     @Autowired
     ProfesionalRepositorio profesionalRepositorio;
 
-    @GetMapping("/database")
+    @Autowired
+    PacienteServicio pacienteServicio;
+
+    @Autowired
+    PacienteRepositorio pacienteRepositorio;
+
+    @GetMapping("/profesionalRandom")
     public String cargarDataBase() throws MiException {
 
         String[] nombres = {"Juan", "José", "Antonio", "Manuel", "David", "Carlos", "Francisco", "Jesús", "Pedro", "Pablo", "Luis", "Diego", "Andrés", "Alejandro", "Miguel", "Fernando", "Jorge", "Óscar", "María", "Ana", "Laura", "Carmen", "Elena", "Isabel", "Marta", "Julia", "Sara", "Paula", "Inés", "Teresa", "Cristina", "Blanca", "Rocío", "Alba", "Daniela", "Andrea"};
@@ -83,9 +92,6 @@ public class UtilController {
             diasDisponibles.add(diaAleatorio);
         }
 
-
-
-
         // Array de LocalTime para los horarios de entrada (de 8:00 a 16:00 cada media hora)
         LocalTime[] horariosEntrada = new LocalTime[17];
         for (int i = 0; i < 17; i++) {
@@ -97,7 +103,6 @@ public class UtilController {
         for (int i = 0; i < 17; i++) {
             horariosSalida[i] = LocalTime.of(12 + i / 2, (i % 2) * 30);
         }
-
 
         LocalTime horarioEntrada = horariosEntrada[random.nextInt(horariosEntrada.length)];
         LocalTime horarioSalida = horariosSalida[random.nextInt(horariosSalida.length)];
@@ -112,6 +117,57 @@ public class UtilController {
         var registrado = profesionalRepositorio.buscarPorEmail(email);
 
         turnoServicio.generarTurnos(registrado);
+
+        return "redirect:../listaProfesionales";
+    }
+
+    @GetMapping("/pacienteRandom")
+    public String cargarDataBasePaciente() throws MiException {
+
+        String[] nombres = {"Juan", "José", "Antonio", "Manuel", "David", "Carlos", "Francisco", "Jesús", "Pedro", "Pablo", "Luis", "Diego", "Andrés", "Alejandro", "Miguel", "Fernando", "Jorge", "Óscar", "María", "Ana", "Laura", "Carmen", "Elena", "Isabel", "Marta", "Julia", "Sara", "Paula", "Inés", "Teresa", "Cristina", "Blanca", "Rocío", "Alba", "Daniela", "Andrea"};
+        String[] apellidos = {"García", "López", "García", "López", "Martínez", "Rodríguez", "Sánchez", "González", "Pérez", "Álvarez", "Romero", "Gómez", "Fernández", "Hernández", "Díaz", "Muñoz", "Castro", "Jiménez", "Moreno", "Martínez", "Rodríguez", "Sánchez", "González", "Pérez", "Álvarez", "Romero"};
+        String[] localidades = {"La Plata", "Mar del Plata", "Quilmes", "San Fernando del Valle de Catamarca", "San Isidro", "Andalgalá", "Resistencia", "Barranqueras ", "Quitilipi"};
+        String[] direcciones = {"9 de Julio 1995", "Rivadavia 110", "San Martín 44", "Belgrano 5962", "Sarmiento 74", "Mitre 998", "Independencia 785", "Pueyrredón 404", "Córdoba 753", "Santa Fe 620"};
+
+        Random random = new Random();
+
+        String nombre = nombres[random.nextInt(nombres.length)];
+
+        String apellido = apellidos[random.nextInt(apellidos.length)];
+
+        String nombreUsuario = (nombre + apellido.charAt(0)).toLowerCase();
+
+        nombreUsuario = nombreUsuario.replace("á", "a");
+        nombreUsuario = nombreUsuario.replace("ú", "u");
+        nombreUsuario = nombreUsuario.replace("é", "e");
+        nombreUsuario = nombreUsuario.replace("í", "i");
+        nombreUsuario = nombreUsuario.replace("ó", "o");
+        nombreUsuario = nombreUsuario.replace("ñ", "n");
+
+        Long dni = random.nextLong(5000000, 50000000);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, random.nextInt(2024 - 2000) + 2000);
+        calendar.set(Calendar.MONTH, random.nextInt(12));
+        calendar.set(Calendar.DAY_OF_MONTH, random.nextInt(31));
+        Date fechaNacimiento = calendar.getTime();
+
+        String email = nombre.toLowerCase() + apellido.substring(0, 1).toLowerCase() + "@mail.com";
+
+        String password = "123123";
+        String password2 = "123123";
+
+        Provincias provincias = Provincias.values()[random.nextInt(Provincias.values().length)];
+        System.out.println("provincias = " + provincias);
+
+        String localidad = localidades[random.nextInt(localidades.length)];
+        System.out.println("localidad = " + localidad);
+
+        String direccion = direcciones[random.nextInt(direcciones.length)];
+        System.out.println("direccion = " + direccion);
+
+
+        pacienteServicio.crearPaciente(null,nombreUsuario,nombre,apellido,dni,fechaNacimiento,email,password,password2);
 
         return "redirect:../listaProfesionales";
     }
